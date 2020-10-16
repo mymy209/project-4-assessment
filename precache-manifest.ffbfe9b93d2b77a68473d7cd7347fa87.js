1bc29b27d3093ed3891bfe3a18024aea26a5860d
mymy209 <myramaronhime@gmail.com>
2020-10-16 12:57:20 -0700

    Updates

---
+++ precache-manifest.ffbfe9b93d2b77a68473d7cd7347fa87.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b62a67a8aaae28dbd583368a8a917eca",
+    "url": "/project-4-assessment/index.html"
+  },
+  {
+    "revision": "47ac7ce393e95ecb43f3",
+    "url": "/project-4-assessment/static/css/main.b26ee44f.chunk.css"
+  },
+  {
+    "revision": "dea5055eeb204d6cf013",
+    "url": "/project-4-assessment/static/js/2.b53a78e4.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/project-4-assessment/static/js/2.b53a78e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "47ac7ce393e95ecb43f3",
+    "url": "/project-4-assessment/static/js/main.ce64cd68.chunk.js"
+  },
+  {
+    "revision": "5545e705446bcbbf1b27",
+    "url": "/project-4-assessment/static/js/runtime-main.058a2c28.js"
+  }
+]);
